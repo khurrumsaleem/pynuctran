6ce4ccc7c3569c3026e7cfba7c062664fef3d0f7
M. R. Omar <33319386+rabieomar92@users.noreply.github.com>
2021-10-04 16:51:19 +0800

    Update example-2.py

--- example-2.py
+++ example-2.py
@@ -16,7 +16,6 @@
 
 from pynuctran.solver import *
 import numpy as np
-from scipy.sparse.csr import csr_matrix
 
 isotopes = [
     'U238',
@@ -41,9 +40,9 @@ hl = [
     np.log(2) / 7.258E13
 ]
 
-#---------------------------- PI DIST METHOD
+#---------------------------- BUILD THE DEPLETION SCHEME.
 sim = solver(isotope_names=isotopes)
-
+# Register all decay events.
 sim.add_removal(0, hl[0], [-1])
 sim.add_removal(1, hl[1], [ 2])
 sim.add_removal(2, hl[2], [ 3])
@@ -52,13 +51,14 @@ sim.add_removal(4, hl[4], [-1])
 sim.add_removal(5, hl[5], [ 6])
 sim.add_removal(6, hl[6], [ 7])
 sim.add_removal(7, hl[7], [-1])
-
+# Register all absorption events.
 sim.add_removal(0, 1E-4, [ 1])
 sim.add_removal(3, 1E-5, [ 4])
+# Register all fission events.
 sim.add_removal(0, 1E-5*(0.013157), [ 5])
 sim.add_removal(0, 1E-5*(1.11541594E-04), [ 6])
 sim.add_removal(0, 1E-5*(1-1.11541594E-04-0.013157), [-1])
-
+# Prepare the initial weights/concentrations.
 w0 = [
     1E+12,
     0.0,
@@ -70,9 +70,8 @@ w0 = [
     0.0
 ]
 
-
 total_time = 1E5
-steps = int(1E10)
+steps = int(1E20)
 n_final = sim.solve(w0,total_time,steps)
 
 #------------------ OBTAINING REFERENCE SOLUTION [CRAM48]----------------------------
@@ -84,6 +83,6 @@ A = sim.prepare_transmutation_matrix()
 n0 = np.transpose(np.array(w0))
 n_final_cram = CRAM48(A,n0,total_time)
 
-# Prints the output of PyNUCTRAN solver and CRAM48, as well as their relative error.
+# Prints the output of PyNUCTRAN solver and CRAM48, as well as their relative errors.
 for i in range(len(isotopes)):
     print('%+20.14e\t%+20.14e\t%+20.14e' % (n_final[i],n_final_cram[i],(float(n_final[i])-n_final_cram[i])/n_final_cram[i]))
