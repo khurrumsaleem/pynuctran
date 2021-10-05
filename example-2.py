8ffab84b0c8bc455b53c4790b0b100485cfde848
M. R. Omar <33319386+rabieomar92@users.noreply.github.com>
2021-10-05 19:37:23 +0800

    Update example-2.py

--- example-2.py
+++ example-2.py
@@ -74,15 +74,27 @@ total_time = 1E5
 steps = int(1E20)
 n_final = sim.solve(w0,total_time,steps)
 
+
 #------------------ OBTAINING REFERENCE SOLUTION [CRAM48]----------------------------
-# The CRAM solver can be obtained from MIT's CRPG codes repository:
+# The CRAM solver was derived from MIT's CRPG codes repository:
 # https://github.com/mit-crpg/opendeplete/blob/master/opendeplete/integrator/cram.py
+#
+# CRAM method is included in this library for PyNUCTRAN's verifications.
 #------------------------------------------------------------------------------------
 
+# Prepare the transmutation matrix. In CRAM method, this matrix is the so-called
+# matrix A. Recall that CRAM approximates the matrix exponential given in the 
+# formula w(t) = exp(At) w0.
 A = sim.prepare_transmutation_matrix()
-n0 = np.transpose(np.array(w0))
-n_final_cram = CRAM48(A,n0,total_time)
 
-# Prints the output of PyNUCTRAN solver and CRAM48, as well as their relative errors.
+
+n0 = np.transpose(np.array(w0))
+n_final_cram = cram.order48(A,n0,total_time)
+
+# Prints the output of PyNUCTRAN solver and CRAM48, as well as their relative error.
+print('%21s---%21s---%21s' % ('-'*21,'-'*21,'-'*21))
+print('%21s   %21s   %21s' % ('Calculated','Reference (CRAM)', 'Rel. Error'))
+print('%21s---%21s---%21s' % ('-'*21,'-'*21,'-'*21))
 for i in range(len(isotopes)):
-    print('%+20.14e\t%+20.14e\t%+20.14e' % (n_final[i],n_final_cram[i],(float(n_final[i])-n_final_cram[i])/n_final_cram[i]))
+    print('%+20.14e\t%+20.14e\t%+20.14e' % (n_final[i],n_final_cram[i],\
+            (float(n_final[i])-n_final_cram[i])/n_final_cram[i]))
