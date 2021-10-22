733e909dc46fc690d50199355849526ed5e9e5a7
M. R. Omar <33319386+rabieomar92@users.noreply.github.com>
2021-10-22 23:15:27 +0800

    Update sparse.py

--- sparse.py
+++ sparse.py
@@ -4,7 +4,7 @@ info = \
 '''
     This class was initially developed to accomodate fast, high-precision sparse
     matrix multiplications and powers. WARNING! This class does not covers all
-    matrix operations, only basic operations used by PyNUCTRAN are covered, i.e.
+    matrix operations, it only cover the basic operations used by PyNUCTRAN, i.e.
     Multiplication and Powers.
     This class uses the basic Python dictionaries to store data. Sparse matrix
     elements are accessed at an incredible speed via the use of hash table.
@@ -19,7 +19,7 @@ info = \
     SPARSE MULTIPLICATION. Consider sparse matrices A and B. We want to evaluate A*B.
     Firstly, the cartesian products (more or less like a possible combinations) of 
     A.common_column and B.common_row are evaluated for all common index, x. The product
-    of these elements are evaluated, and the value of A[i][x] * B[x][j] will contributes
+    of these elements are evaluated, and the value of A[i][x] x B[x][j] will contributes
     to AB[i][j]. For example,
 
     AB[i][j] = A[i][1]*B[1][j] + A[i][2]*B[2][j] + A[i][3]*B[3][j] + ... 
@@ -30,7 +30,7 @@ info = \
     SPARSE POWER. Suppose we want to evaluate the power of a sparse matrix, i.e. A^n.
     Let n be a large integer number. A naive method is given by,
 
-    A^n = A * A * A * .... (n times)
+    A^n = A x A x A x .... (n times)
 
     Fortunately, this process can be accelerated using the binary decomposition method,
     for instance,
@@ -92,11 +92,11 @@ class smatrix:
         for j in self.common_column.keys():
             L1 = self.common_column.get(j,{})
             L2 = other.common_row.get(j,{})
-            comb = list(product(L1,L2))
- 
-            for comb_pair in comb:
-                    result.addelement(comb_pair[0][0], comb_pair[1][1],
-                      self.data[comb_pair[0]] * other.data[comb_pair[1]])
+            # Prepare the cartesian product of L1 an L2
+            comb = [(xA, xB) for xA in L1.keys() for xB in L2.keys()]
+            for x in comb:
+                    result.addelement(x[0][0], x[1][1],
+                      self.data[x[0]] * other.data[x[1]])
         return result
 
     # Creates a sparse IDENTITY matrix.
@@ -110,13 +110,11 @@ class smatrix:
     # Overrides the matrix power operator **. Implements 
     # the binary decomposition method for matrix power.
     def __pow__(self, power: int) -> 'smatrix':
-        if self.shape[0] != self.shape[1]:
-            print('Fatal error: Matrix power of non-square matrix is encountered.')
-            return None
-        if power == 0:
-            return smatrix.identity(self.shape[0])
-        tmp = self.__pow__(power//2)
-        if (power % 2):
-            return self * tmp * tmp
-        else:
-            return tmp * tmp
+        c = self
+        res = smatrix.identity(self.shape[0])
+        while power > 0:
+            if power & 1:
+                res = res * c
+            c = c * c
+            power >>= 1
+        return res
