867d2b80990240ed4ddeee4fa0796243f34fea62
M. R. Omar <33319386+rabieomar92@users.noreply.github.com>
2021-10-22 15:05:11 +0800

    Update and rename ex3_full_endfb71.py to ex2_full_endfb71.py

--- ex2_full_endfb71.py
+++ ex2_full_endfb71.py
@@ -4,17 +4,12 @@
   Time step: 2.84E+4 (2 days).
    
 """
-
-from pynuctran.solver import *
-import numpy as np
-from scipy.sparse.csr import csr_matrix
-
 # Create a list of species involved in the depletion problem.
 # It is possible to include all nuclides, but it is memory expensive.
 # To include all nuclides replace the definition of isotopes with the
 # following:
 #
-# To include the entire 3000++ nuclides (this requires calculation on a legit machine.
+# To include the entire 3000++ nuclides (this prefers calculation on a powerful machine.
 # isotopes = depletion_scheme.get_all_species_names('path\\to\\chains_endfb71.xml')
 # 
 # To include range of nuclides, with mass number between AMin and AMax:
@@ -41,7 +36,7 @@ rxn_rates = {
 }
 
 # Build the depletion scheme based on the nuclides data stored in chains_endfb71.xml.
-depletion_scheme.build_chains(sim, rxn_rates)
+depletion_scheme.build_chains(sim, rxn_rates,'E:\\chain_endfb71.xml')
 
 # Setup the initial concentrations.
 w0 = {
@@ -51,7 +46,6 @@ w0 = {
 # Runs the calculation.
 total_time = 2.84E+4
 n_final = sim.solve(w0, total_time)
-
 
 
 #------------------ OBTAINING REFERENCE SOLUTION [CRAM48]----------------------------
@@ -74,6 +68,6 @@ n_final_cram = cram.order48(A,n0,total_time)
 # Prints the output of PyNUCTRAN solver and CRAM48, as well as their relative error.
 print('%-5s   %-5s   %-21s   %-21s   %-21s' % ('ID', 'Name','Calculated','Reference (CRAM)', 'Rel. Error'))
 for i in range(len(isotopes)):
-    if n_final[i] > 1E-15:
-        print('%i\t%s\t%+20.14e\t%+20.14e\t%+20.14e' % (i, isotopes[i],n_final[i],n_final_cram[i],\
-                ((n_final[i])-n_final_cram[i])/n_final_cram[i]))
+    if n_final[isotopes[i]] > 1E-15:
+        print('%i\t%s\t%+20.14e\t%+20.14e\t%+20.14e' % (i, isotopes[i],n_final[isotopes[i]],n_final_cram[i],\
+                (float(n_final[isotopes[i]])-n_final_cram[i])/n_final_cram[i]))
